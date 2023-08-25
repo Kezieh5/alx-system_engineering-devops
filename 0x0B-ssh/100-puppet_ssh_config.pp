a9e0224d89476d1d14c41dbd7a690ef1caf3d1eb
Kezieh5 <mwangikezzieh@gmail.com>
2023-08-25 01:24:29 -0700

    0x0B-ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# using puppet to make changes
+
+file { '/etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+}
